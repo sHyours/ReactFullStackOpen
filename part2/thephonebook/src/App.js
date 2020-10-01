edef46e04a7676a53652c6a6d468e994fded1437
sHyours <jie_boky@yeah.net>
2020-10-01 16:17:06 +0900

    Exercises 2.11 compeleted

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setPhoneNumber] = useState('')
   const [search, setSearch] = useState('')
-
   const addPersons = (event) => {
     event.preventDefault()
     console.log('add', event.target);
@@ -34,6 +31,11 @@ const App = () => {
   const searchChange = (e) => {
     setSearch(e.target.value);
   }
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }, [])
   return (
     <div>
       <h2>Phonebook</h2>
