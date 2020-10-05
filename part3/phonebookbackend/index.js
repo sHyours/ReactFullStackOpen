f25a847743d3f7a70725e211d847ee22b27b5929
sHyours <jie_boky@yeah.net>
2020-10-05 15:17:29 +0900

    Exercises 3.7.-3.8. compeleted

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,7 +1,9 @@
-const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+morgan.token('body',(req)=>JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const PORT = 3001
 let persons = [
     {
