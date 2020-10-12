c87a7867888812de29f827b0eb7200f40ed67e59
sHyours <jie_boky@yeah.net>
2020-10-12 14:23:01 +0900

    Exercises 3.11. completed

---
+++ part3/phonebookbackend/build/precache-manifest.223e905850f73c07febfc6111743e076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fe5f47c0c92ab36bb5dfba8e7fd5f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "337390d13f4f3274c0a3",
+    "url": "/static/css/main.43c15ae7.chunk.css"
+  },
+  {
+    "revision": "0b786274b792fc803667",
+    "url": "/static/js/2.fd1a695e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd1a695e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337390d13f4f3274c0a3",
+    "url": "/static/js/main.db637131.chunk.js"
+  },
+  {
+    "revision": "c771fc46209c33cf411a",
+    "url": "/static/js/runtime-main.3a2acdf3.js"
+  }
+]);
--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static("build"))
 morgan.token('body',(req)=>JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const PORT = process.env.PORT || 3001
